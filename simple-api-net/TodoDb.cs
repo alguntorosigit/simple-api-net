ba37857238421ecb21a7de6c2867bff8d3ed7549
alguntorosigit <alsigitguntoro@gmail.com>
2023-02-24 10:25:51 +0700

    Add project files.

---
+++ simple-api-net/TodoDb.cs
@@ -0,0 +1,11 @@
+﻿namespace simple_api_net
+{
+    using Microsoft.EntityFrameworkCore;
+    public class TodoDb : DbContext
+    {
+        public TodoDb(DbContextOptions<TodoDb> options) 
+            : base(options) { }
+
+        public DbSet<Todo> Todos => Set<Todo>();
+    }
+}
